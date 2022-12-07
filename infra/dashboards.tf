0da5fd0cf93ef56c283652d357aeb6ba9c34ca8a
mojibhashemi <mojib_hashemi@hotmail.com>
2022-12-08 00:44:59 +0100

    Legge til resten av widgets i dashboards.tf

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -41,6 +41,44 @@ resource "aws_cloudwatch_dashboard" "main" {
         "region": "eu-west-1",
         "title": "Total sum med penger i handlekurve"
       }
+    },
+    {
+      "type": "metric",
+      "x": 1,
+      "y": 1,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.candidate_id}",
+            "checkouts.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Totalt antall handlevogner er blitt sjekket ut"
+      }
+    },
+    {
+      "type": "metric",
+      "x": 1,
+      "y": 1,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.candidate_id}",
+            "checkout_latency.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Gjennomsnittlig responstid for Checkout metoden i Controller-klassen."
+      }
     }
   ]
 }
