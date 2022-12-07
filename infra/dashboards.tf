c6cb7f2e89ccbc4e2084fbf556513c5c53aa2de9
mojibhashemi <mojib_hashemi@hotmail.com>
2022-12-07 20:11:24 +0100

    Legge til cartsvalue.value i dashboards.tf

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -22,6 +22,25 @@ resource "aws_cloudwatch_dashboard" "main" {
         "region": "eu-west-1",
         "title": "Total number of carts"
       }
+    },
+    {
+      "type": "metric",
+      "x": 1,
+      "y": 1,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.candidate_id}",
+            "cartsvalue.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Total sum med penger i handlekurve"
+      }
     }
   ]
 }
