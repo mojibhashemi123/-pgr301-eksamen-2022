334cdfde06fc4360cec853f87dd7b6ca83fdcbf3
mojibhashemi <mojib_hashemi@hotmail.com>
2022-12-08 15:51:01 +0100

    redigere dashboards.tf

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -44,15 +44,15 @@ resource "aws_cloudwatch_dashboard" "main" {
     },
     {
       "type": "metric",
-      "x": 1,
-      "y": 1,
+      "x": 0,
+      "y": 0,
       "width": 12,
       "height": 6,
       "properties": {
         "metrics": [
           [
             "${var.candidate_id}",
-            "checkouts.value"
+            "checkouts.count"
           ]
         ],
         "period": 300,
@@ -63,8 +63,8 @@ resource "aws_cloudwatch_dashboard" "main" {
     },
     {
       "type": "metric",
-      "x": 1,
-      "y": 1,
+      "x": 0,
+      "y": 0,
       "width": 12,
       "height": 6,
       "properties": {
